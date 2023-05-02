eb70a4ae07a44194f144725fd48fd6b7c499c75b
Asker <kotsevasker0@gmail.com>
2023-05-02 20:16:00 +0300

    Initial Commit

---
+++ LoginApp/LoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppApp.swift
+//  LoginApp
+//
+//  Created by ASKER on 02.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct LoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
